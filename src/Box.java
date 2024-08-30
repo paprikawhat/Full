d09b0d4b6495322ee0dc586708cf2f25bdfbdafb
papri <paprika.git@gmail.com>
2024-08-30 22:30:19 +0300

    second

--- src/Box.java
+++ src/Box.java
@@ -1,2 +1,43 @@
-public class Box {
+class Box {
+    private double height; // ОБЪЯВЛЕНИЕ ПАРАМЕТРОВ
+    private double width;
+    private double depth;
+    double volume() { // КОНСТРУКТОР ДЛЯ КЛОНИРОВАНИЯ
+        return height * width * depth;
+    }
+    Box(Box ob) {
+        height = ob.height;
+        width = ob.width;
+        depth = ob.depth;
+    }
+    Box() { // КОНСТРУКТОР ПО-УМОЛЧАНИЮ
+        height = -1;
+        width = -1;
+        depth = -1;
+    }
+    Box(double h, double w, double d) { // КОНСТРУКТОР С УКАЗАНИЕМ РАЗМЕРОВ
+        height = h;
+        width = w;
+        depth = d;
+    }
+
+}
+class BoxShip extends Box { // РАСШИРЯЮЩИЙ КЛАСС
+    double weight; // ДОБАВЛЕНИЕ ПАРАМЕТРОВ
+    boolean isEasy;
+    BoxShip(BoxShip ob) { // КОНСТРУКТОР ДЛЯ КЛОНИРОВАНИЯ
+        super(ob);
+        weight = ob.weight;
+        isEasy = ob.isEasy;
+    }
+    BoxShip(double h, double w, double d, double m, boolean s) { //КОНСТРУКТО С УКАЗАНИЕМ ВСЕХ ПАРАМЕТРОВ
+        super(h, w, d);
+        weight = m;
+        isEasy = s;
+    }
+    BoxShip() { //КОНСТРУКТОР ПО-УМОЛЧАНИЮ
+        super();
+        weight = -1;
+        isEasy = false;
+    }
 }
---
+++ src/Stack.java
@@ -0,0 +1,20 @@
+public class Stack {
+    int[] stack = new int[];
+    int tos = -1;
+    void push (int num) {
+        if (num == 9) {
+            System.out.println("Стек заполнен.");
+        } else {
+            stack[++tos] = num;
+        }
+    }
+
+    int pop() {
+            if (tos <=0) {
+                System.out.println("Стек пустю");
+                return 0;
+            } else {
+                return stack[tos--];
+        }
+    }
+}
