e61fdd200755d9ff15209c13c0ee9e5603509845
papri <paprika.git@gmail.com>
2024-09-02 21:23:51 +0300

    Exceptions

--- src/CreateBox.java
+++ src/CreateBox.java
@@ -2,12 +2,8 @@ public class CreateBox {
     public static void main(String[] args) {
         System.out.println("Корбка создвна");
         Box newBox = new Box(50, 20, 30, 500);
-        System.out.println("Размеры: 30 x 50 x 20\nВес:500");
-        if (newBox.isEasy) {
-            System.out.println("Отправить!");
-        } else {
-            System.out.println("Сложность с отправлением.");
-        }
-
+        newBox.param();
+        Box newBox2 = new Box();
+        newBox2.param();
     }
 }
