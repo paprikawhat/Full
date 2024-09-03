6e8de5e84247849054a98769202d8870d8ae2373
papri <paprika.git@gmail.com>
2024-09-04 02:06:59 +0300

    Exceptions

--- src/Box.java
+++ src/Box.java
@@ -1,21 +1,31 @@
-public class Box {
-    double height; // ОБЪЯВЛЕНИЕ ПАРАМЕТРОВ
-    double width;
-    double depth;
-    double weight; // ДОБАВЛЕНИЕ ПАРАМЕТРОВ
-    boolean isEasy;
-    float price;
-    double volume() { // МЕТОД ВЫЧИСЛЕНИЯ ОБЪЁМА
-        return height * width * depth;
+class Box {
+    // ОБЪЯВЛЕНИЕ ПАРАМЕТРОВ И МЕТОДОВ ОБРАЩЕНИЯ С ПАРАМЕТРАМИ
+    private double height;
+    void h(double n) {height = n;}
+    private double width;
+    void w(double n) {width = n;}
+    private double depth;
+    void d(double n) {depth = n;}
+    private double weight;
+    void wh(double n) {weight = n;}
+    private float price;
+    void p(float n) {price = n;}
+    private boolean isEasy;
+    boolean e() {isEasy = (weight <= 10000) && (height+width+depth < 150) && (height < 100 && width < 100 && depth < 100);
+        return isEasy;
+    }
+    void volume() {
+        double v = height*width*depth;
+        System.out.println("Объём коробки: " + v/1000 + " литров");
     }
     void param() {
-        System.out.println("Размеры: " + height + " x " + width + " x " + depth + "\nВес:" + weight);
-        if (this.isEasy) {
+        System.out.println("Размеры: " + height + " x " + width + " x " + depth + "\nВес: " + weight/1000 + " кг.");
+        if (isEasy) {
             System.out.println("Отправить!");
         } else {
             System.out.println("Сложность с отправлением.");
         }
-        System.out.println("Цена отправления: "); //
+        System.out.println("Цена отправления: ");
     }
     Box(Box ob) { // КОНСТРУКТОР ДЛЯ КЛОНИРОВАНИЯ
         height = ob.height;
@@ -30,14 +40,14 @@ public class Box {
         width = -1;
         depth = -1;
         weight = -1;
-        isEasy = true;
+        price = 0;
     }
     Box(double h, double w, double d, double wh) { // КОНСТРУКТОР С УКАЗАНИЕМ РАЗМЕРОВ
         height = h;
         width = w;
         depth = d;
         weight = wh;
-        isEasy = weight <= 1000 || h + w + d > 150 || h > 150 || w > 150  || d > 150;
+        isEasy = (wh <= 10000) && (h+w+d < 150) && (h < 100 && w < 100 && d < 100);
     }
 
 }
---
+++ src/Exception/FinallyDemo.java
@@ -0,0 +1,42 @@
+package Exception;
+
+public class FinallyDemo {
+    // Сгенерировать исключение в нутри метода .
+    static void procA() {
+        try {
+            System.out.println("Внутри метода procA()");
+            throw new RuntimeException("демонстрация");
+        }
+        finally {
+            System.out.println("Блок finally метода procA()");
+        }
+    }
+    // Возвратить управление изнутри блока try .
+    static void procB() {
+        try {
+            System.out.println("Внутри метода procB ( ) ");
+            return;
+        } finally {
+            System.out.println("Блок finally метода procB ()");
+        }
+    }
+    // Выполнить блок try обычным образом .
+    static void procC() {
+        try {
+            System.out.println("Внутри метода procC ( ) ");
+        } finally {
+            System.out.println("Блок finally метода procC().");
+        }
+    }
+    public static void main( String[] args) {
+        try {
+            procA() ;
+        } catch (Exception e) {
+            System.out.println("Исключение перехвачено");
+        }
+        procB ();
+        procC();
+    }
+}
+
+
