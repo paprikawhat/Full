92fa7aea527c734bc5b859ddedc5e96e5336870c
papri <paprika.git@gmail.com>
2024-09-05 01:32:11 +0300

    Взаимодействие потоков

--- src/Threads/Sync.java
+++ src/Threads/Sync.java
@@ -1,4 +1,19 @@
 package src.Threads;
+/* Как только поток входит в какой-либо синхронизированный
+  метод в экземпляре, никакой другой поток не может входить в любой
+  другой синхронизированный метод в том же экземпляре. */
+class Call {
+    // Метод может быть вызван лишь одним потоком за раз
+    synchronized void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Прерван!");
+        }
+        System.out.println("]");
+    }
+}
   // Класс реализующий многопоточность.
   class Caller implements Runnable{
       Call target;
@@ -10,39 +25,28 @@ package src.Threads;
           t = new Thread(this);
       }
       public void run() {
-          target.call(msg);
+        //Синхронизируются
+          synchronized (target) {
+              target.call(msg);
+          }
       }
   }
-  /* Как только поток входит в какой-либо синхронизированный
-    метод в экземпляре, никакой другой поток не может входить в любой
-    другой синхронизированный метод в том же экземпляре. */
-
-class Call {
-  // Метод может быть вызван лишь одним потоком за раз
-    synchronized void call(String msg) {
-        System.out.print("[" + msg);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.out.println("Прерван!");
-        }
-        System.out.println("]");
-    }
-}
 class Sync {
     public static void main(String[] args) {
         Call target = new Call();
         Caller ob1 = new Caller(target, "Hello");
         Caller ob2 = new Caller(target, "Synchronized");
         Caller ob3 = new Caller(target, "World");
-
+      // Запуск потоков.
         ob1.t.start();
         ob2.t.start();
         ob3.t.start();
+      // Ожидание окончаний работы потоков
         try {
             ob1.t.join();
             ob2.t.join();
             ob3.t.join();
+      // Ошибка прерывания потока выполнения.
         } catch (InterruptedException e) {
             System.out.println("Прерван!");
         }
