2035ae9e80ead8067bccffb0974b8834fe168657
papri <paprika.git@gmail.com>
2024-09-05 22:35:22 +0300

    Взаимодействие потоков

---
+++ src/Threads/Queue.java
@@ -0,0 +1,76 @@
+package src.Threads;
+
+// В этом классе методы Q синхронизированы и не могут быть вызваны одновременно.
+class Q {
+    int n;
+    boolean valueSet = false; // Переменная для проверки условия ожидания.
+
+    synchronized void put(int n) {
+        while (valueSet) {
+            try {
+                wait(); // Всегда внутри цикла проверяющего условие ожидания потока.
+            } catch (InterruptedException e) {
+                System.out.println("Перехвачено исключение IE.");
+            }
+        }
+        this.n = n;
+        valueSet = true;
+        System.out.println("Помещено: " + n);
+        notify();
+    }
+    synchronized int get() {
+        while(!valueSet) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Перехвачено исключение IE.");
+            }
+        }
+            System.out.println("Извлечено: " + n);
+            valueSet = false;
+            notify();
+            return n;
+    }
+}
+// Объект для реализации потока производителя.
+class Producer implements Runnable{
+    Q q = new Q();
+    Thread t;
+    Producer(Q q) {
+        this.q = q;
+        t = new Thread(this, "Producer");
+    }
+    @Override //
+    public void run() {
+        int i = 0;
+        while(true) { // Поток бесконечного выполнения.
+            q.put(i++);
+        }
+    }
+}
+// Объект для реализации потока производителя
+class  Consumer implements Runnable{
+    Q q;
+    Thread t;
+    Consumer(Q q) {
+        this.q = q;
+        t = new Thread(this, "Consumer");
+    }
+
+    @Override
+    public void run() {
+        while(true) { // Поток бесконечного выполнения.
+            q.get();
+        }
+    }
+}
+public class Queue {
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+        p.t.start();
+        c.t.start();
+        System.out.println("Ctrl-F2 to stop.");
+    }
+}
