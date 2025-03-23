354bc95266bc92fb4d1477121b28a4f881e2e829
papri <paprika.git@gmail.com>
2025-03-23 20:37:18 +0300

    Chapter_II Maps and Comparators

--- src/Chapter_II/ComparatorDemo.java
+++ src/Chapter_II/ComparatorDemo.java
@@ -3,10 +3,13 @@ package src.Chapter_II;
 
 import java.util.*;
 
-class MyComp implements Comparator<String> {
+class CompByLastName implements Comparator<String> {
     public int compare(String aStr, String bStr) {
+        //Защита от попытки присвоить Null
+        if(!aStr.contains(" ") || !bStr.contains(" ")) {
+            return aStr.compareToIgnoreCase(bStr);
+        }
         int i, j , k;
-        // Небезопасный поиск кидает исключение при отсутствии пробела в строке
         i = aStr.lastIndexOf(' ');
         j = bStr.lastIndexOf(' ');
         k = aStr.substring(i).compareToIgnoreCase(bStr.substring(j));
@@ -15,33 +18,59 @@ class MyComp implements Comparator<String> {
         } else return k;
     }
 }
+class CompByFirstName implements Comparator<String> {
+    public int compare(String aStr, String bStr) {
+       return aStr.compareToIgnoreCase(bStr);
+    }
+}
+class CompBySalary implements Comparator<Double> {
+    public int compare (Double a, Double b) {
+        return a.compareTo(b);
+    }
+}
+
 public class ComparatorDemo {
     public static void main(String[] args) {
         System.out.println("  *******************");
-        TreeMap<String, Double> myTreeMap = new TreeMap<>(new MyComp());
-        myTreeMap.put("Alph Murz", 1900.0);
-        myTreeMap.put("Sytastus Zoetghard", 4500.0);
-        myTreeMap.put("Kont Jahoviche", 9100.0);
-        myTreeMap.put("Bulk Murz", 1400.0);
-        myTreeMap.put("Zoender Kiest", 2000.0);
+        Comparator<String> compByLastNameThenFirst =
+                new CompByLastName().thenComparing(new CompByFirstName());
+        TreeMap<String, Double> myTreeMap = new TreeMap<>(compByLastNameThenFirst);
+        {
+            myTreeMap.put("Alph Murz", 1900.0);
+            myTreeMap.put("Sytastus Zoetghard", 4500.0);
+            myTreeMap.put("Kont Jahoviche", 9100.0);
+            myTreeMap.put("Bulk Murz", 1700.0);
+            myTreeMap.put("Zoender Alz", 32000.0);
+            myTreeMap.put("Yotie Kits", 2400.0);
+            myTreeMap.put("Charles Lavas", 1800.0);
+            myTreeMap.put("Dart Shiest", 5600.0);
+            myTreeMap.put("Cloth Fault", 8800.0);
+            myTreeMap.put("Synth Heilt", 4500.0);
+            myTreeMap.put("Vard Vardensen", 9100.0);
+            myTreeMap.put("Buk Quin", 1600.0);
+            myTreeMap.put("Robert Alz", 900.0);
+            myTreeMap.put("Bender Dots", 4300.0);
+            myTreeMap.put("Gaven Shoes", 2500.0);
+            myTreeMap.put("Bartolomey Est", 5000.0);
+        }
         // Получение набора элементов
         Set<Map.Entry<String, Double>> set = myTreeMap.entrySet();
         for (Map.Entry<String, Double> me : set) {
-            System.out.print(STR."\{me.getKey()} ");
+            System.out.print(STR."\{me.getKey()}: $");
             System.out.println(me.getValue());
         }
         System.out.println("  *******************");
         double balanceOfBulk = myTreeMap.get("Bulk Murz");
-        System.out.println(STR."New balance = \{balanceOfBulk + 1000}");
+        System.out.println(STR."New Bulk balance = \{balanceOfBulk + 1000}");
         System.out.println("  *******************");
         Comparator<String> mc = String::compareTo;
-        TreeSet<String> treeSet = new TreeSet<>(mc);
+        TreeSet<String> treeSet = new TreeSet<>(mc.reversed());
         treeSet.add("C");
         treeSet.add("B");
         treeSet.add("A");
         treeSet.add("E");
         treeSet.add("D");
-        for(String e : treeSet) {
+        for (String e : treeSet) {
             System.out.println(STR."\{e} ");
         }
     }
